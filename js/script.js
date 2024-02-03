f1553f4703f3b0981dc3124099bc63aa6f3a71e8
Isabella Martinelli <69216574+icxmartinelli@users.noreply.github.com>
2024-02-02 22:35:33 -0300

    cpf digit and birthday validation, starting to customize the error messages

--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
-import ehUmCPF from "./valida-cpf"; // chamando a function de valida-cpf.js
-const camposDoFormulario = documento.querySelectorAll("[required]");
+import ehUmCPF from "./valida-cpf.js"; // chamando a function de valida-cpf.js
+import ehMaiorDeIdade from "./valida-idade.js";
+const camposDoFormulario = document.querySelectorAll('[required]');
+
 // seleciona todos elementos 'required'
 camposDoFormulario.forEach((campo) => { // para cada um dos campos 'required'
-    campo.addEventListener("blur", () => veificaCampo(campo)); // ele vai esperar pelo 'blur' que é quando tira o foco do input
+    campo.addEventListener("blur", () => verificaCampo(campo)); // ele vai esperar pelo 'blur' que é quando tira o foco do input
+    campo.addEventListener("invalid", evento => evento.preventDefault()); // ele oculta a mensagem padrão do HTML 5 para campo inválido campo.validity = false em alguma das propriedades de validação
 });
+
+const tiposDeEro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'tooShort',
+    'customError'
+]
+
+const mensagens = {  // chave porque é objeto
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caracteres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caracteres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
+}
+
 
 function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo); // podemos usar como função daqui porque importamos
+    }
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
     }
 }
--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,6 +1,10 @@
 export default function ehUmCPF(campo) { // exportada como padrão quando esse arquivo é chamado
     const cpf = campo.value.replace(/\.|-/g, ""); // está removendo '.' e '-'
-    validaNumerosRepetidos(cpf);
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("rapaz, esse cpf não existe");
+    } else {
+        console.log("sucesso, existe");
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -16,5 +20,43 @@ function validaNumerosRepetidos(cpf) {
         '88888888888',
         '99999999999'
     ]
+    // console.log("repetido " + numerosRepetidos.includes(cpf) + " " + `${cpf.length}`);
     return numerosRepetidos.includes(cpf); // vai percorrer a lista e se achar o cpf retorna verdadeiro
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    // console.log("primeiro " + soma != cpf[9] + " " + soma);
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
